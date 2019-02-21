ba97159d8d18834bd50a4c3edc5548e129ff8129
CyrilGa <cgaiton@excilys.com>
2019-02-21 11:39:42 +0100

    add AddCOmputerServlet

--- src/main/java/fr/cgaiton611/servlet/AddComputerServlet.java
+++ src/main/java/fr/cgaiton611/servlet/AddComputerServlet.java
@@ -1,5 +1,43 @@
 package fr.cgaiton611.servlet;
 
-public class AddComputerServlet {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.cgaiton611.service.ComputerService;
+
+@WebServlet(urlPatterns = { "addComputer" })
+public class AddComputerServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private ComputerService computerService = ComputerService.getInstance();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/ressources/views/addComputer.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("computerName");
+		String introduced = request.getParameter("introduced");
+		String discontinued = request.getParameter("discontinued");
+		String companyId = request.getParameter("companyId");
+		if (name != null) {
+			computerService.create(name, introduced, discontinued, companyId)
+		}
+		response.sendRedirect(request.getContextPath() + "/addComputer");
+		
+	}
 
 }
