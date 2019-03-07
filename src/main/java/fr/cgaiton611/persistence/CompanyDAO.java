4cf77f3f08ed4c20437bddbccface3ca7b9966b0
CyrilGa <cgaiton@excilys.com>
2019-03-07 14:31:55 +0100

    change url in errors jsp

--- src/main/java/fr/cgaiton611/persistence/CompanyDAO.java
+++ src/main/java/fr/cgaiton611/persistence/CompanyDAO.java
@@ -9,12 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import com.zaxxer.hikari.HikariDataSource;
 
 import fr.cgaiton611.model.Company;
 
--- src/main/java/fr/cgaiton611/persistence/DAO.java
+++ src/main/java/fr/cgaiton611/persistence/DAO.java
@@ -7,8 +7,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.zaxxer.hikari.HikariDataSource;
-
 
 /**
  * Abstract Class getting the connection with the database and define CRUD
